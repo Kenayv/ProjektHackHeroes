9e430f2c156ed5efaf8d1b8199e48b65cc795603
mew6210 <mew.62@wp.pl>
2023-10-20 21:29:13 +0200

    proszedzialajteraz

--- main.py
+++ main.py
@@ -1,71 +1,74 @@
 from kivy.app import App
-from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.layout import Layout
-from kivy.uix.boxlayout import BoxLayout
+
+from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 from kivy.uix.button import Button
-# Create both screens. Please note the root.manager.current: this is how
-# you can control the ScreenManager from kv. Each screen has by default a
-# property manager that gives you the instance of the ScreenManager used.
-
-sm = ScreenManager()
-#screens = [Screen(name='Title {}'.format(i)) for i in range(4)]
-screens=[]
-screens[0]=(Screen(name="first"))
-screens[1]=(Screen(name="second"))
-sm.switch_to(screens[0])
+from kivy.uix.boxlayout import BoxLayout
 
 
-# Declare both screens
-class MenuScreen(Screen):
-    pass
+#
+#
+#WOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+#
+#
 
-class SettingsScreen(Screen):
-    pass
 
-class TestApp(App):
-    
-    
+
+
+sm = ScreenManager(transition=FadeTransition())
+
+class SwitchToApp(App):
+    #cos pod tym jest zle, napraw wracanie albo zrob to inaczej idk
+    def do_switch_to_settings(self, *instance):
+        scr2 = Screen(name='settings')
         
+        layout=BoxLayout(orientation="horizontal")
+        button1=Button(text="menu")
+        button1.bind(on_press=self.build)
+        button2=Button(text="ustawienia")
         
-    def callback(self,instance):
-        sm.current="second"
-        print("XD")
-        layout=BoxLayout(orientation="vertical")
-        
-        
-        button1=Button(text="dosc")
         
         layout.add_widget(button1)
-        
-        return layout
-
-
-
-    def build(self):
+        layout.add_widget(button2)
         
         
+        
+        
+        scr2.add_widget(layout)
+        sm.switch_to(scr2)
+        
+        
+        
+        
+        return sm
+    
+    
+    
+    def build(self,*instance):
+        scr1 = Screen(name='menu')
+        button1=Button(text="change")
+        button1.bind(on_press=self.do_switch_to_settings)
+        button2=Button(text="JAZDAAAA")
         layout=BoxLayout(orientation="vertical")
-        
-        
-        button1=Button(text="xd?")
-        button1.bind(on_press=self.callback)
+        #   RZECZY TU
         layout.add_widget(button1)
+        layout.add_widget(button2)
+        
+        
+        
+        #
+        scr1.add_widget(layout)
+        
+        
+        sm.add_widget(scr1)
+        
+        
+        return sm
+
+    
         
 
-        return layout
-    
-    
-    
-   
-    
-    
-    
-    
-    
-    
-    
-    
 
 if __name__ == '__main__':
-    TestApp().run()
+    
+    SwitchToApp().run()
