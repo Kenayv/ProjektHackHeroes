06e6fba19316dbb067e87ad5a988fa6f7aa747c9
Kaspir7fox <kkaspirowicz@gmail.com>
2023-11-01 20:40:31 +0100

    fishki

--- lib/pages/flash_cards_page.dart
+++ lib/pages/flash_cards_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'dart:io';
+import 'dart:math';
+
 class FlashCardsPage extends StatefulWidget {
   @override
   _FlashCardsPageState createState() => _FlashCardsPageState();
@@ -81,5 +84,152 @@ class _FlashCardsPageState extends State<FlashCardsPage> {
         ),
       ),
     );
+  }
+}
+
+
+
+class FlashCardSet {
+  String SetName = "";
+  List<String> a = [];
+  List<String> b = [];
+
+  FlashCardSet(this.SetName){}
+
+  void addFlashCard() {
+    String textA = stdin.readLineSync()!; 
+    a.add(textA);
+
+    String textB = stdin.readLineSync()!;
+    b.add(textB);
+  }
+
+  void addFlashCardDev(String TextA, String TextB) { //nie wiem które będzie lepsze do implementacji w fluttezrze wiec są 2
+    a.add(TextA);
+    b.add(TextB);
+  }
+
+  void learn(){
+    int i = 0;
+    do{
+      print(a[i]);
+      print("N - next; C - Change");
+      String input = stdin.readLineSync()!;
+      if(input == "C"){
+        print(b[i]);
+        print("N - next; C - Change");
+        String input = stdin.readLineSync()!;
+        if(input == "C"){
+      }else{i++;}
+      }else{i++;}
+      
+    }while(a.length > i);
+  }
+
+  void learnDev(){
+    bool WasALast = false;
+    int input;
+
+      List<String> AorB(){
+        if(WasALast){
+          WasALast = false;
+          return b;
+        }else{
+          WasALast = true;
+          return a;
+        }
+      }
+
+      int i = 0;
+      do{
+        WasALast = false;
+        do{
+        print(AorB()[i]);
+        print("1 - next; 2 - Change"); //do usunięcia dołączająć do apki ofc
+        input = int.parse(stdin.readLineSync()!); //cza zmienić 1 jako swipe i 2 jako tap na przykład
+        }while(input == 2); 
+        i++;
+      }while(a.length > i);
+  }
+
+  void testAmount(int amount){
+    int correct = 0;
+    for(int i=1;i<=amount;i++){
+      int rand = Random().nextInt(a.length);
+      print(a[rand]);
+      stdin.readLineSync()!;
+      print(b[rand]);
+      print("Press C if correct");
+      String isCorrect = stdin.readLineSync()!;
+      if(isCorrect=="C"){
+        correct++;
+      }
+    }
+    print("Correct:");
+    print(correct);
+    print("Inorrect:");
+    print(amount - correct);
+  }
+
+  void testShuffled(){
+    int correct = 0;
+    List<String> c = a;
+    List<String> d = b;
+
+      Random random = Random();
+
+  for (int i = c.length - 1; i > 0; i--) {
+    int j = random.nextInt(i + 1);
+
+    String tempA = c[i];
+    c[i] = c[j];
+    c[j] = tempA;
+
+    String tempB = d[i];
+    d[i] = d[j];
+    d[j] = tempB;
+  }
+
+    for(int i=0;i<c.length;i++){
+      print(c[i]);
+      stdin.readLineSync()!;
+      print(d[i]);
+      print("Press C if correct");
+      String isCorrect = stdin.readLineSync()!;
+      if(isCorrect=="C"){
+        correct++;
+      }
+    }
+    print("Correct:");
+    print(correct);
+    print("Inorrect:");
+    print(d.length - correct);
+  }
+
+  void testTimed(int Time){ //Time is in miliseconds
+    int correct = 0;
+    int counter = 0;
+    DateTime endTime = DateTime.now().add(Duration(milliseconds: Time));
+      while(DateTime.now().isBefore(endTime)){
+        int rand = Random().nextInt(a.length);
+        print(a[rand]);
+        stdin.readLineSync()!;
+        print(b[rand]);
+        print("Press C if correct");
+        String isCorrect = stdin.readLineSync()!;
+        if(isCorrect=="C"){
+          correct++;
+      }
+      counter++;
+    }
+    print("Correct:");
+    print(correct);
+    print("Inorrect:");
+    print(counter - correct);
+  }
+
+  void removeById(int Id){
+    a.removeAt(Id);
+    b.removeAt(Id);
   }
 }
