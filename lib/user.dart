e4df41e432e27e471c75078dc655ccea4dc1a96e
Kenayv <grosicki.jan@gmail.com>
2023-11-04 15:46:33 +0100

    update

--- lib/user.dart
+++ lib/user.dart
@@ -15,21 +15,28 @@ class User {
   late int _statsLongestStreak;
 
   //user variables, will be stored in SharedPreferences
-  late DateTime _lastLearnedDate;
-  late bool _learnedToday;
-  late bool _tasksFinishedToday;
+  late DateTime _varLastLearnedDate;
+  late DateTime _varLastDailyGoalCompletionDate;
+  late int _varTasksFinishedToday;
+  late int _varFlashCardsFinishedToday;
+  late bool _varDailyGoalAchieved;
 
   //user config variables, will be stored in SharedPreferences
   late int _configPrefHour;
   late int _configPrefDailyTasks;
   late int _configPrefDailyFlashCards;
 
+  //  -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
+
   User() {
     _loadStats();
+    _loadVars();
     _loadConfig();
   }
 
+  //
   //  -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
+  //
 
   //Loads user stats and saves them as variables in User class.
   Future<void> _loadStats() async {
@@ -43,12 +50,43 @@ class User {
     _statsDayStreak = userStats.getInt('dayStreak') ?? 0;
   }
 
+  //  -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
+
+  Future<void> _loadVars() async {
+    final SharedPreferences userVars = await SharedPreferences.getInstance();
+
+    final lastLearnedYear = userVars.getInt('lastLearnedYear') ?? 1999;
+    final lastLearnedMonth = userVars.getInt('lastLearnedMonth') ?? 1;
+    final lastLearnedDay = userVars.getInt('lastLearnedDay') ?? 1;
+    _varLastLearnedDate = DateTime(lastLearnedYear, lastLearnedMonth, lastLearnedDay);
+
+    if (_varLastLearnedDate.day != DateTime.now().day) {
+      _varTasksFinishedToday = 0;
+      _varFlashCardsFinishedToday = 0;
+    }
+
+    final lastGoalYear = userVars.getInt('lastGoalYear') ?? 1999;
+    final lastGoalMonth = userVars.getInt('lastGoalMonth') ?? 1;
+    final lastGoalDay = userVars.getInt('lastGoalDay') ?? 1;
+
+    _varDailyGoalAchieved = _isDailyGoalCompleted(lastGoalYear, lastGoalMonth, lastGoalDay);
+    if (!_varDailyGoalAchieved && lastGoalDay != DateTime.now().day - 1) _statsDayStreak = 0;
+  }
+
+  //  -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
+
   //Loads user config and saves them as variables in User class.
   Future<void> _loadConfig() async {
     final SharedPreferences userConfig = await SharedPreferences.getInstance();
 
     _configPrefHour = userConfig.getInt('configPrefHour') ?? 18;
+    _configPrefDailyTasks = userConfig.getInt('configPrefDailyTasks') ?? 5;
+    _configPrefDailyFlashCards = userConfig.getInt('configPrefFlashCards') ?? 20;
   }
+
+  //
+  //  -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -
+  //
 
   //Saves user stats as SharedPrefs.
   Future<void> _saveStats() async {
@@ -63,15 +101,41 @@ class User {
     userStats.setInt('dayStreak', _statsDayStreak);
   }
 
-  //Saves user stats as SharedPrefs.
-  Future<void> _saveConfig() async {
-    //WARNING: this function might be very sub-optimal, because it is invoked on every User variable change. If it happens to be visibly laggy, only changed variable should be updated as SharedPref. For now, the definition can stay as it is for the sake of simplicity.
-    final SharedPreferences userConfig = await SharedPreferences.getInstance();
-
-    userConfig.setInt('configPrefHour', _configPrefHour);
-  }
-
+  //
   //  -   -   -   -   ↓ Functions changing user's variables ↓    -   -   -   -   -   -
+  //
+
+  bool _isDailyGoalCompleted(lastGoalYear, lastGoalMonth, lastGoalDay) {
+    _varLastDailyGoalCompletionDate = DateTime(lastGoalYear, lastGoalMonth, lastGoalDay);
+
+    switch (_varLastDailyGoalCompletionDate.day - DateTime.now().day) {
+      case 0:
+        return true;
+
+      case 30:
+        if (_varLastDailyGoalCompletionDate.month == DateTime.january ||
+            _varLastDailyGoalCompletionDate.month == DateTime.march ||
+            _varLastDailyGoalCompletionDate.month == DateTime.may ||
+            _varLastDailyGoalCompletionDate.month == DateTime.july ||
+            _varLastDailyGoalCompletionDate.month == DateTime.august ||
+            _varLastDailyGoalCompletionDate.month == DateTime.october ||
+            _varLastDailyGoalCompletionDate.month == DateTime.december) return true;
+
+      case 29:
+        if (_varLastDailyGoalCompletionDate.month == DateTime.april ||
+            _varLastDailyGoalCompletionDate.month == DateTime.june ||
+            _varLastDailyGoalCompletionDate.month == DateTime.september ||
+            _varLastDailyGoalCompletionDate.month == DateTime.november) return true;
+
+      case 28:
+        if (_varLastDailyGoalCompletionDate.year % 4 == 0 && _varLastDailyGoalCompletionDate.month == DateTime.february)
+          return true;
+
+      case 27:
+        if (_varLastDailyGoalCompletionDate.month == DateTime.february) return true;
+    }
+    return false;
+  }
 
   updateFCRushHighScore(int highScore) {
     if (highScore <= 0) return;
