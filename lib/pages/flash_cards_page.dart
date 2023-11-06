dfefc6b53770319e1bed189f872f10d971812240
Kenayv <grosicki.jan@gmail.com>
2023-11-06 19:32:18 +0100

    adding flashcards and flashCardSets working!

--- lib/pages/flash_cards_page.dart
+++ lib/pages/flash_cards_page.dart
@@ -86,7 +86,7 @@ class FlashCardSet {
     return _flashCards[randomIndex];
   }
 
-  String getSetName() {
+  String getName() {
     return fcSetName;
   }
 
@@ -136,6 +136,12 @@ class _FlashCardsPageState extends State<FlashCardsPage> {
     });
   }
 
+  void saveFlashCardSets() {
+    for (int i = 0; i < flashCardSets.length; i++) {
+      flashCardSets[i]._saveFlashCards();
+    }
+  }
+
   Future<void> loadFlashCardSets() async {
     try {
       Directory appDocDir = await getApplicationDocumentsDirectory();
@@ -168,79 +174,236 @@ class _FlashCardsPageState extends State<FlashCardsPage> {
     loadFlashCardSets().then((_) {
       // Debug print statements
       print('Number of flashCardSets: ${flashCardSets.length}');
+      setState(() {}); // Trigger a rebuild after loading data.
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder<void>(
-        future: loadFlashCardSets(), // Call the function here
-        builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            // Now you can safely access flashCardSets
-            FlashCard? testowa = flashCardSets.isNotEmpty ? flashCardSets[0].getRandFlashCard() : null;
-
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 200,
-                    height: 400,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(color: Colors.black),
-                    ),
-                    child: Center(
-                      child: AnimatedCrossFade(
-                        duration: Duration(milliseconds: 300),
-                        crossFadeState: isFlipped ? CrossFadeState.showSecond : CrossFadeState.showFirst,
-                        firstChild: Text(
-                          testowa?.front ?? 'Front text not available',
-                          style: TextStyle(fontSize: 20),
-                        ),
-                        secondChild: Text(
-                          testowa?.back ?? 'Back text not available',
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(height: 20),
-                  if (!showRatingButtons)
-                    ElevatedButton(
-                      onPressed: flipCard,
-                      child: Text('Flip Card'),
-                    ),
-                  if (showRatingButtons)
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {
-                            flipCard();
-                          },
-                          child: Text('Dobrze'),
-                        ),
-                        SizedBox(width: 20),
-                        ElevatedButton(
-                          onPressed: () {
-                            flipCard();
-                          },
-                          child: Text('Źle'),
-                        ),
-                      ],
-                    ),
-                ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Twoje zestawy fiszek:',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
-            );
-          } else {
-            // Handle loading state, for example, display a loading indicator
-            return Center(child: CircularProgressIndicator());
-          }
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: flashCardSets.length,
+              itemBuilder: (context, index) {
+                FlashCardSet flashCardSet = flashCardSets[index];
+                return _buildFlashCardSetItem(flashCardSet);
+              },
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _addFlashCardDialog();
         },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+
+  Future<dynamic> _addFlashCardDialog() {
+    TextEditingController inputName = TextEditingController();
+    TextEditingController inputFront = TextEditingController();
+    TextEditingController inputBack = TextEditingController();
+    String? selectedValue = "";
+    List<String> fcSetNames = [];
+    for (int i = 0; i < flashCardSets.length; i++) {
+      fcSetNames.add(flashCardSets[i].getName());
+    }
+    const String newSetVal = "Nowy...";
+    fcSetNames.add(newSetVal);
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Dodaj Fiszkę'),
+          content: StatefulBuilder(builder: (context, setState) {
+            return Column(
+              children: <Widget>[
+                DropdownButtonFormField<String>(
+                  decoration: InputDecoration(labelText: 'Zestaw fiszek'),
+                  items: fcSetNames.map((theme) {
+                    return DropdownMenuItem<String>(
+                      value: theme,
+                      child: Text(theme),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    setState(() {
+                      selectedValue = value;
+                    });
+                  },
+                ),
+                Visibility(
+                  visible: selectedValue == newSetVal,
+                  child: TextField(
+                    controller: inputName,
+                    decoration: InputDecoration(labelText: 'Nazwa'),
+                  ),
+                ),
+                Visibility(
+                  visible: selectedValue != newSetVal,
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: inputFront,
+                        decoration: InputDecoration(labelText: 'Przód karty'),
+                      ),
+                      TextField(
+                        controller: inputBack,
+                        decoration: InputDecoration(labelText: 'Tył karty'),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          }),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Anuluj'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Dodaj'),
+              onPressed: () {
+                if (selectedValue == newSetVal) {
+                  flashCardSets.add(FlashCardSet(fcSetName: inputName.text));
+                } else {
+                  final int index = flashCardSets.indexWhere((flashCardSet) => flashCardSet.fcSetName == selectedValue);
+                  if (index == -1) Navigator.of(context).pop();
+                  flashCardSets[index].addFlashCard(inputFront.text, inputBack.text);
+                }
+                saveFlashCardSets();
+                setState(() {});
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildFlashCardSetItem(FlashCardSet flashCardSet) {
+    final randomColor = Color.fromRGBO(
+      Random().nextInt(256),
+      Random().nextInt(256),
+      Random().nextInt(256),
+      1.0,
+    );
+
+    return ListTile(
+      title: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Container(
+          height: 64,
+          decoration: BoxDecoration(
+            color: randomColor,
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  flashCardSet.getName(),
+                  style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Ilość kart: ${flashCardSet.getLength()}',
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
 }
+
+
+// return Scaffold(
+    //   body: FutureBuilder<void>(
+    //     future: loadFlashCardSets(), // Call the function here
+    //     builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+    //       if (snapshot.connectionState == ConnectionState.done) {
+    //         // Now you can safely access flashCardSets
+    //         FlashCard? testowa = flashCardSets.isNotEmpty ? flashCardSets[0].getRandFlashCard() : null;
+
+    //         return Center(
+    // child: Column(
+    //   mainAxisAlignment: MainAxisAlignment.center,
+    //   children: [
+    //     Container(
+    //       width: 200,
+    //       height: 400,
+    //       decoration: BoxDecoration(
+    //         color: Colors.white,
+    //         border: Border.all(color: Colors.black),
+    //       ),
+    //       child: Center(
+    //         child: AnimatedCrossFade(
+    //           duration: Duration(milliseconds: 300),
+    //           crossFadeState: isFlipped ? CrossFadeState.showSecond : CrossFadeState.showFirst,
+    //           firstChild: Text(
+    //             testowa?.front ?? 'Front text not available',
+    //             style: TextStyle(fontSize: 20),
+    //           ),
+    //           secondChild: Text(
+    //             testowa?.back ?? 'Back text not available',
+    //             style: TextStyle(fontSize: 20),
+    //           ),
+    //         ),
+    //       ),
+    //     ),
+    //     SizedBox(height: 20),
+    //     if (!showRatingButtons)
+    //       ElevatedButton(
+    //         onPressed: flipCard,
+    //         child: Text('Flip Card'),
+    //       ),
+    //     if (showRatingButtons)
+    //       Row(
+    //         mainAxisAlignment: MainAxisAlignment.center,
+    //         children: [
+    //           ElevatedButton(
+    //             onPressed: () {
+    //               flipCard();
+    //             },
+    //             child: Text('Dobrze'),
+    //           ),
+    //           SizedBox(width: 20),
+    //           ElevatedButton(
+    //             onPressed: () {
+    //               flipCard();
+    //             },
+    //             child: Text('Źle'),
+    //           ),
+    //         ],
+    //       ),
+    //   ],
+    // ),
+    // );
+    //   } else {
+    //     // Handle loading state, for example, display a loading indicator
+    //     return Center(child: CircularProgressIndicator());
+    //   }
+    // },
+    //   ),
+    // );
